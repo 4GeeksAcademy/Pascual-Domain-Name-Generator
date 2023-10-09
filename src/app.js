d567049c210e64a70356c22e3a53e9070f61277e
Pascual Vila <46532433+pascual143@users.noreply.github.com>
2023-10-09 19:29:10 +0000

    for loop

--- src/app.js
+++ src/app.js
@@ -7,5 +7,18 @@ import "./assets/img/4geeks.ico";
 
 window.onload = function() {
   //write your code here
-  console.log("Hello Rigo from the console!");
+  let pronoun = ["the", "our"];
+  let adj = ["great", "big"];
+  let noun = ["jogger", "racoon"];
+  let ext = [".com", ".net", ".es"];
+
+  for (let index = 0; index < pronoun.length; index++) {
+    for (let index1 = 0; index1 < adj.length; index1++) {
+      for (let index2 = 0; index2 < noun.length; index2++) {
+        for (let index3 = 0; index3 < noun.length; index3++) {
+          console.log(pronoun[index] + adj[index1] + noun[index2] + ".com");
+        }
+      }
+    }
+  }
 };
