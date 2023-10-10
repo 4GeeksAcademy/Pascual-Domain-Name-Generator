bc52b59e1c8c6d9827ffebe4d47fa4db08d30a38
Pascual Vila <46532433+pascual143@users.noreply.github.com>
2023-10-10 11:04:40 +0000

    adding extensions

--- src/app.js
+++ src/app.js
@@ -15,8 +15,8 @@ window.onload = function() {
   for (let index = 0; index < pronoun.length; index++) {
     for (let index1 = 0; index1 < adj.length; index1++) {
       for (let index2 = 0; index2 < noun.length; index2++) {
-        for (let index3 = 0; index3 < noun.length; index3++) {
-          console.log(pronoun[index] + adj[index1] + noun[index2] + ".com");
+        for (let index3 = 0; index3 < ext.length; index3++) {
+          console.log(pronoun[index] + adj[index1] + noun[index2] + ext[index3]);
         }
       }
     }
